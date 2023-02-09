aab73d96f2ca911d88a720012cc428aa415fcf2c
Roman <cikipuki10@gmail.com>
2023-02-09 19:34:08 +0200

    function declaration

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict'
 
-const numberOfFilms = prompt(' How many movies do you watched? ')
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt(' How many movies do you watched?', '')
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt(' How many movies do you watched? ', '')
+    }
+}
+
+start()
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,32 +20,50 @@ const personalMovieDB = {
     print: false,
 }
 
+function rememberMyFilms () {
+    for ( let i = 0; i < 1; i++ ) {
 
-for ( let i = 0; i < 1; i++ ) {
-
-    const lastMovie = prompt('One of the last films?'),
-          scoreMovie = prompt('How much would you rate?', 0);
-
-    if ( lastMovie !== null 
-        && scoreMovie !== null 
-        && lastMovie !== '' 
-        && scoreMovie !== '' 
-        && lastMovie.length < 50 ) {
-
-        personalMovieDB.movies[lastMovie] = scoreMovie
-    } else {
-        i--
+        const lastMovie = prompt('One of the last films?').trim(),
+              scoreMovie = prompt('How much would you rate?', 0);
+    
+        if ( lastMovie !== null 
+            && scoreMovie !== null 
+            && lastMovie !== '' 
+            && scoreMovie !== '' 
+            && lastMovie.length < 50 ) {
+    
+            personalMovieDB.movies[lastMovie] = scoreMovie
+        } else {
+            i--
+        }
     }
 }
 
-if ( personalMovieDB.count < 10 ) {
-    console.log('few movies');
-} else if ( personalMovieDB.count > 10 && personalMovieDB.count < 30 ) {
-    console.log('cool audience');
-} else if ( personalMovieDB.count >= 30  ) {
-    console.log('fan');
-} else {
-    console.log('error');
+rememberMyFilms()
+
+function detectedPersonalLevel () {
+    
+    if ( personalMovieDB.count < 10 ) {
+        console.log('few movies');
+    } else if ( personalMovieDB.count > 10 && personalMovieDB.count < 30 ) {
+        console.log('cool audience');
+    } else if ( personalMovieDB.count >= 30  ) {
+        console.log('fan');
+    } else {
+        console.log('error');
+    }
+
 }
 
-console.log(personalMovieDB);  
+
+
+function writeYourGenres (favoriteGenre) {
+
+    for ( let i = 0; i < 3; i++ ) {
+
+        favoriteGenre.push(prompt(` Your favorite genre? ${i + 1} `))
+    }
+
+}
+
+writeYourGenres(personalMovieDB.genres)
